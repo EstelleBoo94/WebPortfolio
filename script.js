09cda57b6202c50c5d4ef64ca4e316307dbcb348
Estelle <estelleboo94@gmail.com>
2025-02-20 10:51:15 +0100

    Add thank you page and background image; update form submission redirect

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 document.getElementById("menu-toggle").addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent the link from refreshing the page
+    event.preventDefault();
     document.getElementById("menu").classList.toggle("hidden");
 });
 
+document.addEventListener("click", function(event) {
+    var menu = document.getElementById("menu");
+    var menuToggle = document.getElementById("menu-toggle");
+    
+    if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
+        if (!menu.classList.contains("hidden")) {
+            menu.classList.add("hidden");
+        }
+    }
+});
