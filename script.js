e496f140bffcee9fd266794a72f8ebdd9018d0a4
Estelle <estelleboo94@gmail.com>
2025-02-21 00:39:52 +0100

    Add weather data display and placeholder CV file; update index and style

--- script.js
+++ script.js
@@ -13,3 +13,35 @@ document.addEventListener("click", function(event) {
         }
     }
 });
+
+const weatherData = document.querySelector("#weather-data");
+const weatherDataText = document.querySelector("#weather-data-text");
+
+fetch("https://api.open-meteo.com/v1/forecast?latitude=62.3913&longitude=17.3063&current=temperature_2m,apparent_temperature,is_day,weather_code&forecast_days=1")
+.then(response => {
+    if(!response.ok){
+        throw new Error("Gick inte att hämta väderdata.")
+    }
+
+    return response.json();
+})
+.then(data => {
+    const icon = document.createElement("i");
+
+    if(data.current.weather_code == 2 || data.current.weather_code == 3){
+        icon.classList.add("fa-solid", "fa-cloud", "footer-weather-icon")
+    }
+    
+    const ptag = document.createElement("p");
+    ptag.textContent = `${data.current.temperature_2m}°C | känns som ${data.current.apparent_temperature}°C`
+
+    weatherData.insertBefore(icon, weatherData.firstChild)
+    weatherDataText.appendChild(ptag);
+})
+.catch(error => {
+    return console.log("fel inträffat", error)
+})
+
+// data.current.temperature_2m
+// data.current.weather_code
+// data.current.apparent_temperature
